048f30b989ab36b183bae1a84f7f0e26512fdb19
lakshmic6 <31327239+lakshmic6@users.noreply.github.com>
2017-10-29 01:53:48 +0530

    commit
    commit

---
+++ Lab assignment 9/source/Rest Service/MongoConnection.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mnpw3d on 20/10/2016.
+ */
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://<dbuser>:<dbpassword>@ds237815.mlab.com:37815/lab9';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+/*
+url :   mongodb://root:secure@ds161483.mlab.com:61483/asefall17
+ */
+
